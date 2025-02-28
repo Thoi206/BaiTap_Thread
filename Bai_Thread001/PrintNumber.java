825c0cf6456b5e74f3fc9499970b181d8fcc4b58
Thoi206 <thoiidol22@gmail.com>
2025-02-28 20:09:53 +0700

    Update PrintNumber.java

--- Bai_Thread001/PrintNumber.java
+++ Bai_Thread001/PrintNumber.java
@@ -8,14 +8,14 @@ class PrintNumber {
         while (number < LIMIT) {
             while (number % 2 == 0) {
                 try {
-                    wait(); // Chờ đến khi số lẻ
+                    wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             System.out.println("Thread 1: " + number);
             number++;
-            notify(); // Đánh thức thread chẵn
+            notify();
         }
     }
 
@@ -23,14 +23,14 @@ class PrintNumber {
         while (number <= LIMIT) {
             while (number % 2 == 1) {
                 try {
-                    wait(); // Chờ đến khi số chẵn
+                    wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             System.out.println("Thread 2: " + number);
             number++;
-            notify(); // Đánh thức thread lẻ
+            notify();
         }
     }
 }
